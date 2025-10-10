8f94511009bda9d2df03f3e5fd9dd4a4491da2bd
Aysegul-hub <ayseguldelialioglu20@gmail.com>
2025-10-10 15:38:49 +0300

    update0

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private static Object ilkBosluk;
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        StaticLifetime obj1 = new StaticLifetime();
+        StaticLifetime obj2 = new StaticLifetime();
 
-        System.out.println("Hello World");
-        System.out.println("Hoşgeldin Ayşegül Delialioğlu");
-        // unutmamak için yeniden çalışıyorum.
-        /*deneme mesaj
+        obj1.incrementCounter();
+        obj2.incrementCounter();
 
-
-         */
-
-
-        int n = 5;
-        int b = 2;
-        int ayse;
-        ayse = 15;
-        System.out.println("merhaba ayse " + b);
-        float pi = 3.14F; //float yapımında sonuna F yaz ya da double yap
-        String name = "benim adım aysegul";
-        System.out.println(name);
-        double sayi = 3.14;
-        System.out.println(sayi);
-        name = name.toUpperCase();
-        int ilkBosluk = name.indexOf(" ");
-        System.out.println(ilkBosluk);
-
-        String a = "benim adım aysegul";
-        a = a.toUpperCase();
-        a = a.toLowerCase();
-        int firstGaps = a.indexOf('m'); //indexOf(" ") kalıbı int ile tanımlıdır çünkü bize bir sayı değeri döndürür.
-        String firstWord = a.substring(0, firstGaps); //substring(something) kalıbı ilk boşluktan sonrasınık yazdırdı
-        int lastGaps = a.lastIndexOf(" ");
-        String lastWord = a.substring(lastGaps);
-
-        System.out.println(lastWord);
-
-        System.out.println(firstWord);
-        System.out.println(firstGaps);
-        System.out.println("8. karakter: " + a.charAt(8));
-        System.out.println("8. karakter: ");
-
-
+    }
 
 
     }
 
 
-}
--- src/StaticLifetime.java
+++ src/StaticLifetime.java
@@ -7,13 +7,6 @@ public class StaticLifetime {
         System.out.println(" The value of counter : " + counter);
 
     }
-    public static void main(String[] args){
-        StaticLifetime obj1 = new StaticLifetime();
-        StaticLifetime obj2 = new StaticLifetime();
 
-        obj1.incrementCounter();
-        obj2.incrementCounter();
-
-    }
 
 }
