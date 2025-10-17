1195c210affea46846ec2775f20965f92ddd30c6
Aysegul-hub <ayseguldelialioglu20@gmail.com>
2025-10-17 14:32:14 +0300

    update1

---
+++ src/CheckPrime.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class CheckPrime {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter an integer please: ");
+        int number = scan.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number!");
+
+        }
+        else {
+            System.out.println(number + " is not a prime number !");
+
+        }
+        scan.close();
+
+
+    }
+
+
+    public static boolean isPrime(int num){
+        if (num <= 1) {
+            return false;
+
+        }
+        for ( int i = 2 ; i <=num /2 ; i++) {
+            if ( num % i == 0 ){
+                return false;
+
+            }
+        }
+        return true;
+    }
+
+}
