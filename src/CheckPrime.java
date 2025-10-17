5d5b985b6dee509d460f2e52acce0dc6caa60191
Aysegul-hub <ayseguldelialioglu20@gmail.com>
2025-10-17 14:32:50 +0300

    update0

--- src/CheckPrime.java
+++ src/CheckPrime.java
@@ -27,7 +27,7 @@ public class CheckPrime {
             return false;
 
         }
-        for ( int i = 2 ; i <=num/2 ; i++) {
+        for ( int i = 2 ; i <= num/2 ; i++) {
             if ( num % i == 0 ){
                 return false;
 
